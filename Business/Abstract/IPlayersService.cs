b24d3d04fa2a1a859f7a7f7f29737247d43d78f9
Alperen <alperenoz1464@gmail.com>
2021-03-01 19:05:57 +0300

    Layers done except API

---
+++ Business/Abstract/IPlayersService.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IPlayersService
+    {
+        Player AddPlayer(Player player);
+        List<Player> GetPlayers();
+        Player FindPlayer(int id);
+        Player UpdatePlayer(Player player);
+        void DeletePlayer(int id);
+    }
+}
---
+++ Business/Concrete/PlayersService.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.NewFolder
+{
+    public class PlayersService : IPlayersService
+    {
+        IPlayersDAL _playersDal;
+        public PlayersService(IPlayersDAL playersDal)
+        {
+            _playersDal = playersDal;
+        }
+
+        public Player AddPlayer(Player player)
+        {
+            return _playersDal.AddPlayer(player);
+        }
+
+        public void DeletePlayer(int id)
+        {
+            _playersDal.DeletePlayer(id);
+        }
+
+        public Player FindPlayer(int id)
+        {
+            return _playersDal.FindPlayer(id);
+        }
+
+        public List<Player> GetPlayers()
+        {
+            return _playersDal.GetPlayers();
+        }
+
+        public Player UpdatePlayer(Player player)
+        {
+            return _playersDal.UpdatePlayer(player);
+        }
+    }
+}
