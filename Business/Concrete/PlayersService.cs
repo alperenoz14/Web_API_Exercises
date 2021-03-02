a32df97e16938bbc64f17dcf60241b7311a1d4ca
Alperen <alperenoz1464@gmail.com>
2021-03-02 23:35:58 +0300

    WebAPI layer done

--- Business/Concrete/PlayersService.cs
+++ Business/Concrete/PlayersService.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Business.NewFolder
+namespace Business.Concrete
 {
     public class PlayersService : IPlayersService
     {
--- WebAPI/Controllers/TeamController.cs
+++ WebAPI/Controllers/TeamController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Business.Abstract;
+using Business.Concrete;
+using Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,6 +14,40 @@ namespace WebAPI.Controllers
     [ApiController]
     public class TeamController : ControllerBase
     {
+        private IPlayersService _IPlayerService;
+        public TeamController(IPlayersService playersService)
+        {
+            _IPlayerService = playersService;
+        }
 
+        [HttpPost]
+        public Player Add([FromBody]Player player)
+        {
+            return _IPlayerService.AddPlayer(player);
+        }
+
+        [HttpGet]
+        public List<Player> GetAll()
+        {
+            return _IPlayerService.GetPlayers();
+        }
+
+        [HttpGet("{id}")]
+        public Player Find(int id)
+        {
+            return _IPlayerService.FindPlayer(id);
+        }
+
+        [HttpPut]
+        public Player Update([FromBody]Player player)
+        {
+            return _IPlayerService.UpdatePlayer(player);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _IPlayerService.DeletePlayer(id);
+        }
     }
 }
